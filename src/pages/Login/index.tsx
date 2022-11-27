0a2122e61e8625b8ca6e3f50e0dc09159526489b
Fernandes <fernandes.r1999@gmail.com>
2022-11-27 17:04:19 -0300

    WIP Auth

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,36 @@
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+import { useEffect } from 'react';
+
+import { Button } from 'react-native';
 import { Container } from "../../styles/global"
 import { Text } from "./styles"
 
+WebBrowser.maybeCompleteAuthSession();
+const useProxy = false;
+const redirectUri = AuthSession.makeRedirectUri({
+    useProxy,
+});
+
 export const LoginPage = () => {
+    const discovery = AuthSession.useAutoDiscovery('https://25.73.21.13:44310/');
+    const [request, result, promptAsync] = AuthSession.useAuthRequest(
+        {
+          clientId: 'native.code',
+          redirectUri,
+          scopes: ['openid', 'profile', 'email', 'offline_access'],
+        },
+        discovery
+    );
+
+    useEffect(() => {
+        if (!result) return;
+        console.log(result);
+    }, [result]);
+
     return (
         <Container>
-            <Text>Login Page</Text>
+            <Button title="Login!" disabled={!request} onPress={() => promptAsync({ useProxy })} />
             <Text>Login Page</Text>
             <Text>Login Page</Text>
         </Container>
